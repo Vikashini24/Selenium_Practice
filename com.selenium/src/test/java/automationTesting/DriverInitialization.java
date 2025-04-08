6fd6e587de0d379240494b84d59f25ca5b251639
Kanniya Vikashini <kanniyavikashini@gmail.com>
2025-04-08 09:32:56 +0530

    select dropdown

--- com.selenium/src/test/java/automationTesting/DriverInitialization.java
+++ com.selenium/src/test/java/automationTesting/DriverInitialization.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -35,7 +36,6 @@ public class DriverInitialization {
 		phone.sendKeys("9876543210");
 		WebElement address = driver.findElement(By.id("textarea"));
 		address.sendKeys("Coimbatore");
-		System.out.println("Textboxes are filled");
 	}
 	
 	@Test (priority=2)
@@ -62,13 +62,21 @@ public class DriverInitialization {
 		}
 	}
 	
+	@Test (priority=4)
+	public void selectDropdown() {
+		WebElement country = driver.findElement(By.id("country"));
+		Select dropdown = new Select(country);
+		dropdown.selectByVisibleText("United Kingdom");
+	}
 	
 	
-//	@AfterSuite
-//	public void driverDriver() {
-//		if(driver!=null) {
-//			driver.quit();
-//		}
-//	}
+	
+	
+	@AfterSuite
+	public void driverDriver() {
+		if(driver!=null) {
+			driver.quit();
+		}
+	}
 	
 }
