f7faeb647446923add63a0381d175e65edc28391
Kanniya Vikashini <kanniyavikashini@gmail.com>
2025-04-10 08:19:40 +0530

    autosuggestive dropdown

--- com.selenium/src/test/java/practice/HiddenDropdown.java
+++ com.selenium/src/test/java/practice/HiddenDropdown.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -31,7 +32,7 @@ public class HiddenDropdown {
 	}
 	
 	@Test (priority=1)
-	public void login() {
+	public void autosuggestiveDropdown() {
 		WebElement userName = driver.findElement(By.name("username"));
 		userName.sendKeys("Admin");
 		WebElement password = driver.findElement(By.name("password"));
@@ -40,23 +41,27 @@ public class HiddenDropdown {
 		loginButton.click();
 		WebElement clickPIM = driver.findElement(By.linkText("PIM"));
 		clickPIM.click();
+		WebElement dropdown = driver.findElement(By.xpath("//label[text()='Job Title']/ancestor::div[@class='oxd-grid-item oxd-grid-item--gutters']//div/div[@class='oxd-select-text-input']"));
+		dropdown.sendKeys("Automation Tester" + Keys.ENTER);
 	}
 	
-	@Test (priority=2)
-	public void hiddenDropdownElement() {
-		WebElement dropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[text()='Job Title']/ancestor::div[@class='oxd-grid-item oxd-grid-item--gutters']//div[@class='oxd-select-text oxd-select-text--active']")));
-		dropdown.click();
-		//	WebElement dropdown = driver.findElement(By.xpath("//label[text()='Job Title']/ancestor::div[@class='oxd-grid-item oxd-grid-item--gutters']//div[@class='oxd-select-text oxd-select-text--active']"));
-		//dropdown.sendKeys("Automation Tester");
-		List<WebElement> dropdownList = driver.findElements(By.xpath("//div[@role='lilstbox']"));
-		for(WebElement selectDropdown : dropdownList) {
-			String list = selectDropdown.getText();
-			if(list.equalsIgnoreCase("Automation Tester")) {
-				selectDropdown.click();
-				break;
-			}
-		}
-	}
+//	@Test (priority=2)
+//	public void hiddenDropdownElement() {
+//		//WebElement dropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[text()='Job Title']/ancestor::div[@class='oxd-grid-item oxd-grid-item--gutters']//div[@class='oxd-select-text oxd-select-text--active']")));
+//		//WebElement dropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[text()='Job Title']/ancestor::div[@class='oxd-grid-item oxd-grid-item--gutters']//div/div[@class='oxd-select-text-input']")));
+//		//dropdown.click();
+//		//	WebElement dropdown = driver.findElement(By.xpath("//label[text()='Job Title']/ancestor::div[@class='oxd-grid-item oxd-grid-item--gutters']//div[@class='oxd-select-text oxd-select-text--active']"));
+////		WebElement dropdown = driver.findElement(By.xpath("//label[text()='Job Title']/ancestor::div[@class='oxd-grid-item oxd-grid-item--gutters']//div/div[@class='oxd-select-text-input']"));
+////		dropdown.sendKeys("Automation Tester" + Keys.ENTER);
+////		List<WebElement> dropdownList = driver.findElements(By.xpath("//div[@role='lilstbox']"));
+////		for(WebElement selectDropdown : dropdownList) {
+////			String list = selectDropdown.getText();
+////			if(list.equalsIgnoreCase("Automation Tester")) {
+////				selectDropdown.click();
+////				break;
+////			}
+////		}
+//	}
 	
 //	@AfterSuite
 //	public void quitDriver() {
