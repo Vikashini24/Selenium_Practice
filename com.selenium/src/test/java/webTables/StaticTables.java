44bf37acc6574e794ddd7eb9b1855eca5aaf2af1
Kanniya Vikashini <kanniyavikashini@gmail.com>
2025-04-18 08:14:00 +0530

    static table

---
+++ com.selenium/src/test/java/webTables/StaticTables.java
@@ -0,0 +1,62 @@
+package webTables;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class StaticTables {
+	
+	WebDriver driver;
+	
+	@BeforeSuite
+	public void initializeDriver() {
+		driver = new ChromeDriver();
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void staticWebTable() {
+		//find total number of rows in a table
+		List<WebElement> tableRows = driver.findElements(By.xpath("//table[@name='BookTable']//tr"));
+		int rowSize = tableRows.size();
+		System.out.println("Number of rows: " + rowSize + "\n");
+		
+		//find total number of columns in a table
+		List<WebElement> tableColumns = driver.findElements(By.xpath("//table[@name='BookTable']//th"));
+		int columnSize = tableColumns.size();
+		System.out.println("Number of columns: " + columnSize + "\n");
+		
+		//read data from specific row and column --> row-5 column-1
+		WebElement rOneCFive = driver.findElement(By.xpath("//table[@name='BookTable']//tr[5]/td[1]"));
+		System.out.println("Row-5, Column-1: " + rOneCFive.getText() + "\n");
+		
+		//read data from all the rows and columns
+		for(int r=2; r<=rowSize; r++) {
+			for(int c=1; c<=columnSize; c++) {
+				WebElement allData = driver.findElement(By.xpath("//table[@name='BookTable']//tr[" + r + "]/td[ " + c + "]"));
+				System.out.print("Data: " + allData.getText() + "\t");
+			}
+			System.out.println("\n");
+		}
+		
+		//print book names whose author is Mukesh
+		for(int r=2; r<rowSize; r++) {
+			WebElement authors = driver.findElement(By.xpath("//table[@name='BookTable']//tr[" + r + "]/td[2]"));
+			String authorName = authors.getText();
+			if(authorName.equals("Mukesh")) {
+				String bookName = driver.findElement(By.xpath("//table[@name='BookTable']//tr[" + r + "]/td[1]")).getText();
+				System.out.println("Books name whose author is Mukesh:" +bookName + "\t" + authorName);
+			}
+		}
+
+		
+		
+	}
+	
+}
